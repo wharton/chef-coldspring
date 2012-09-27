931901f7de317c3f42bdd3f6ba082042162329f3
Courtney Wilburn <wilburnc@wharton.upenn.edu>
2012-09-27 13:14:52 -0400

    initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+# Coldspring Download
+default['coldspring']['install_path'] = "/vagrant/wwwroot"
+default['coldspring']['download']['url'] = "http://www.coldspringframework.org/downloads/coldspring1-2-final.zip"
